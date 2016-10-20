0e9a5329d0cefd794bb5aa2ac3eea857c36d739c
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-10-20 15:38:26 +0100

    for the screen

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,24 +1,20 @@
+# GildedRose class takes an array of items
+# and updates each one by calling instance methods
 class GildedRose
-
   def initialize(items)
     @items = items
   end
 
   def daily_item_update
     @items.each do |item|
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.decrease_sell_in
-        item.update_quality
-      end
+      item.tick if item.name != 'Sulfuras, Hand of Ragnaros'
     end
   end
 end
 
-
+# is a superclass which all other item classes inherit from
 class Item
-
   attr_reader :name, :sell_in, :quality
-
 
   def initialize(name, sell_in, quality)
     @name = name
@@ -31,118 +27,98 @@ class Item
   end
 end
 
-
+# module which has behaviour to instigate to ammend
+# sell_in and quality for items
 module AmmendItems
   MAXIMUM_QUALITY = 50
+
+  def tick
+    decrease_sell_in
+    update_quality
+  end
+
+  private
   def decrease_sell_in
-    @sell_in -= 1 if @sell_in > 0
+    @sell_in -= 1 if sell_in > 0
   end
 
   def increase_quality(amount)
-    @quality += amount if @quality < MAXIMUM_QUALITY
+    @quality += amount if quality < MAXIMUM_QUALITY
   end
 
   def decrease_quality(amount)
-    @quality -= amount if @quality > 0
+    @quality -= amount if quality > 0
   end
 end
 
-
+# is a subclass which inherits from Item.
+# Its specifically for any instances of AgedBrie
 class AgedBrie < Item
   include AmmendItems
 
+  private
   def update_quality
     increase_quality(1)
   end
-
 end
 
-
+# is a subclass which inherits from Item.
+# It adds the behaviour of updating quality for different cases
 class BackstagePass < Item
   include AmmendItems
 
+  private
+
   def update_quality
-    case @sell_in
-    when 0
-      decrease_quality(@quality)
-    when 11..49
-      increase_quality(1)
-    when 6..10
-      increase_quality(2)
-    when 1..5
-      increase_quality(3)
+    if sell_in == 0
+      decrease_quality(quality)
+    else
+      increase_quality(quality_increase_amount)
     end
   end
+
+  def quality_increase_amount
+    case sell_in
+    when 11..49
+      1
+    when 6..10
+      2
+    when 1..5
+      3
+    end
+  end
+
 end
-
-
-
-
+# StandardItem is for items which stick to standard rules
+# decreae of quality at 1 a day up till sell_by daily_item_update
+# then decrease of quality at 2 a day after sell_by date
 class StandardItem < Item
   include AmmendItems
+
+  private
   QUALITY_DEGRADATION_RATE = 1
 
   def update_quality
-      @sell_in > 0 ? decrease_quality(QUALITY_DEGRADATION_RATE) : decrease_quality(QUALITY_DEGRADATION_RATE * 2)
-  end
-end
-
-
-class Conjured < Item
-  include AmmendItems
-  QUALITY_DEGRADATION_RATE = 2
-  def update_quality
-    @sell_in > 0 ? decrease_quality(QUALITY_DEGRADATION_RATE) : decrease_quality(QUALITY_DEGRADATION_RATE * 2)
-  end
-end
-
-
-
-
-=begin
-  def another_method
-    if true == true
-      puts "I shouldn't exist"
-    else item.name != "Sulfuras, Hand of Ragnaros"
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        standard_item(item)
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.sell_in = item.sell_in - 1
-          end
-          if item.sell_in < 0
-            if item.name != "Aged Brie"
-              if item.name != "Backstage passes to a TAFKAL80ETC concert"
-                if item.quality > 0
-                  if item.name != "Sulfuras, Hand of Ragnaros"
-                    item.quality = item.quality - 1
-                  end
-                end
-              else
-                item.quality = item.quality - item.quality
-              end
-            else
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
+    if sell_in > 0
+      decrease_quality(QUALITY_DEGRADATION_RATE)
+    else
+      decrease_quality(QUALITY_DEGRADATION_RATE * 2)
     end
   end
-=end
+end
+
+# StandardItem is for items which stick to standard rules
+# decreae of quality at 1 a day up till sell_by daily_item_update
+# then decrease of quality at 2 a day after sell_by date
+class Conjured < StandardItem
+
+  private
+  QUALITY_DEGRADATION_RATE = 2
+  def update_quality
+    if sell_in > 0
+      decrease_quality(QUALITY_DEGRADATION_RATE)
+    else
+      decrease_quality(QUALITY_DEGRADATION_RATE * 2)
+    end
+  end
+end
--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -1,5 +1,93 @@
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
+describe Item do
+
+  describe "#daily_item_update" do
+    it "does not change the name" do
+      item = StandardItem.new("foo", 0, 0)
+      expect(item.name).to eq "foo"
+    end
+
+    it "degrades quality of normal item by 1" do
+      item = StandardItem.new("foo",10,20)
+      item.tick
+      expect(item.quality).to eq 19
+    end
+
+    it "degrades quality of normal item by 2 when sell_in is at 0" do
+      item = StandardItem.new("foo",0,20)
+      item.tick
+      expect(item.quality).to eq 18
+    end
+
+    it "degrades quality of Conjured item by 2" do
+      item = Conjured.new("Conjured",10,20)
+      item.tick
+      expect(item.quality).to eq 18
+    end
+
+    it "degrades quality of Conuured item by 4 when sell_in is at 0" do
+      item = Conjured.new("foo",0,20)
+      item.tick
+      expect(item.quality).to eq 16
+    end
+
+    it "degrades sell_in of normal item by 1" do
+      item = StandardItem.new("foo",10,20)
+      item.tick
+      expect(item.sell_in).to eq 9
+    end
+
+    it "cannot increase quality of backstage  above 50" do
+      item = BackstagePass.new("Backstage passes to a TAFKAL80ETC concert",10,50)
+      item.tick
+      expect(item.quality).to eq 50
+    end
+
+    it "cannot increase quality of item above 50" do
+      item = AgedBrie.new("Aged Brie",10,50)
+      item.tick
+      expect(item.quality).to eq 50
+    end
+
+    it "increases quality of Brie by 1" do
+      item = AgedBrie.new("Aged Brie",10,20)
+      item.tick
+      expect(item.quality).to eq 21
+    end
+
+
+
+    it "cannot degrade quality below 0" do
+      item = StandardItem.new("foo", 0, 0)
+      item.tick()
+      expect(item.quality).to eq 0
+    end
+
+    it "increase quality of Backstage passes by 2 when sell_in is in between 6 and 10 days" do
+      item = BackstagePass.new("Backstage passes to a TAFKAL80ETC concert", 10, 10)
+      item.tick()
+      expect(item.quality).to eq 12
+    end
+
+    it "increase quality of Backstage passes by 3 when sell_in is in between 1 and 5 days" do
+      item = BackstagePass.new("Backstage passes to a TAFKAL80ETC concert", 4, 10)
+      item.tick()
+      expect(item.quality).to eq 13
+    end
+
+    it "reduces quality of Backstage passes to 0 when sell_in is 0" do
+      item = BackstagePass.new("Backstage passes to a TAFKAL80ETC concert", 0, 10)
+      item.tick()
+      expect(item.quality).to eq 0
+    end
+  end
+
+end
+
+
+
+
 describe GildedRose do
 
   describe "#daily_item_update" do
@@ -19,18 +107,6 @@ describe GildedRose do
       items = [StandardItem.new("foo",0,20)]
       GildedRose.new(items).daily_item_update
       expect(items[0].quality).to eq 18
-    end
-
-    it "degrades quality of Conjured items by 2" do
-      items = [Conjured.new("Conjured",10,20)]
-      GildedRose.new(items).daily_item_update
-      expect(items[0].quality).to eq 18
-    end
-
-    it "degrades quality of Conuured items by 4 when sell_in is at 0" do
-      items = [Conjured.new("foo",0,20)]
-      GildedRose.new(items).daily_item_update
-      expect(items[0].quality).to eq 16
     end
 
     it "degrades sell_in of normal items by 1" do
